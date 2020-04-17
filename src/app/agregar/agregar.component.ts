107034737d7fb8964e21778609f1c09ee6f54eb2
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-16 22:37:39 -0500

    Reactive forms: se agrega FormBuilder

--- src/app/agregar/agregar.component.ts
+++ src/app/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar.component.scss']
 })
 export class AgregarComponent implements OnInit {
+  formularioCreado:FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.crearFormulario();
   }
 
+  crearFormulario(){
+    this.formularioCreado= this.formBuilder.group({
+      nombre: ['Nombre', Validators.required],
+      correo: ['', Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      contrasena: ['', Validators.compose([
+        Validators.required, Validators.minLength(8)
+      ])] 
+    })
+  }
 }
