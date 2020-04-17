4c471600fd8ab8cb8c401764689e73a83a96923e
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-16 21:41:17 -0500

    Reactive forms: se maqueta formulario de agregar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarComponent } from './agregar/agregar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarComponent
   ],
   imports: [
     BrowserModule,
